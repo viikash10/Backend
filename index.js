8822c8bd8638f2a1fcbc4308a94397ec67084041
vikash pal <viikaashpal@gmail.com>
2024-02-13 22:48:29 +0530

    Second Commit

--- index.js
+++ index.js
@@ -4,7 +4,8 @@ const app = express() ;
 const port = 3000; 
 const mongoose = require('mongoose') ;
 const cors = require('cors') ;
-const bcrypt = require('bcrypt') ;
+const bcrypt = require('bcryptjs') ;
+const jwt = require('jsonwebtoken') ;
 
 dotenv.config() ;
 app.use(cors()) ;
@@ -12,12 +13,14 @@ app.use(express.json()) ;
 // app.use(express.urlencoded({ extended: true })) ;
 dotenv.config() ;
 
+const JWT_SECRET = "helloworld" ;
 
 mongoose.connect(process.env.MONGO_URL).then(()=>{console.log("Database Connected!")}).catch((err)=>{console.log(err)})
 
 require('./Model') ;
 const User = mongoose.model("UserInfo") ;
 
+// register route 
 app.post("/register",async(req,res)=>{
     
     const  {name,email,mobile,password} = req.body ;
@@ -44,6 +47,48 @@ app.post("/register",async(req,res)=>{
         res.send({status: "error" ,data: error}) ;
     }
 });
+
+// login route 
+app.post("/login-user",async(req,res)=>{
+    const {email,password } = req.body ;
+    const oldUser = await User.findOne({email:email}) ;   
+    if(!oldUser)
+    {
+        return res.send({data:"User not Exists"}) ;
+    }
+    if(await bcrypt.compare(password,oldUser.password))
+    {
+       const token = jwt.sign({email:oldUser.email},JWT_SECRET) ;
+       console.log(token)
+       if(res.status(201))
+       {
+        return res.send({status: "ok" , data: token});
+       }
+       else{
+        return res.send({error: "error"}) ;  
+       }
+    }
+    else
+    {
+        res.send({status:"error",data:"Invalid Password"}) ;
+    }
+})
+
+
+//user data 
+app.post("/userdata",async(req,res)=>{
+    const {token} = req.body ;
+    try {
+       const user =  jwt.verify(token,JWT_SECRET)  ;
+       const useremail = user.email ;
+
+    User.findOne({email:useremail}).then((data)=>{
+        return res.send({status:"ok",data:data})
+    })
+    } catch (error) {
+        return res.send({error: "error"}) ;  
+    }
+})
 
 
 app.get('/', (req, res) => res.send('Hello World!'));
